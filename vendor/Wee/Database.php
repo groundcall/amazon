3371fcd66ed311af7384e22eb195fd7bb4cad18f
Alexandru Keszeg <akeszeg@pitechnologies.ro>
2013-07-19 13:53:28 +0300

    Database UTF8 by default

--- vendor/Wee/Database.php
+++ vendor/Wee/Database.php
@@ -10,10 +10,14 @@ class Database extends \PDO {
         parent::__construct(sprintf("%s:host=%s;dbname=%s", DB_TYPE, DB_HOST, DB_NAME), DB_USER, DB_PASS);
     }
 
+    /**
+     * @return \PDO
+     */
     public static function sharedInstance() {
 
         if (is_null(self::$instance)) {
             self::$instance = new Database();
+            self::$instance->exec('set names utf8');
         }
 
         return self::$instance;
