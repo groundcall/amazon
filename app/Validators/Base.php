a256d96d922735fa802d38449bf2d181eed87303
Alexandru Keszeg <namlessone@gmail.com>
2013-08-07 20:02:53 +0300

    Fix validatesPresenceOf validator.

--- app/Validators/Base.php
+++ app/Validators/Base.php
@@ -5,7 +5,7 @@ namespace Validators;
 trait Base {
 
     public function validatesPresenceOf($attributeName, $message = "This is a required field") {
-        $this->validate(function($object) use ($attributeName, $message) {
+        $this->registerValidator(function($object) use ($attributeName, $message) {
             $value = $object->getAttribute($attributeName);
 
             if (empty($value)) {
