16ab31ee9f8f003103ee63bec60cc3fc94d86db4
Alexandru Keszeg <akeszeg@pitechnologies.ro>
2013-08-13 15:43:17 +0300

    Disable warnings for now.

--- vendor/Wee/Utils/Utils.php
+++ vendor/Wee/Utils/Utils.php
@@ -8,14 +8,14 @@ class Utils {
      * Returns a celized string from a lowe case and underscored string by upper-casing the first and each letter preceded by an underscore
      */
     static function classify($string) {
-        return preg_replace(array('#/(.?)#e', '/(^|_)(.)/e'), array("'::'.strtoupper('\\1')", "strtoupper('\\2')"), $string);
+        return @preg_replace(array('#/(.?)#e', '/(^|_)(.)/e'), array("'::'.strtoupper('\\1')", "strtoupper('\\2')"), $string);
     }
 
     /**
      * Returns a camelized string from a lower case and underscored string by upper-casing each letter preceded by an underscore.
      */
     static function camelize($string) {
-        return preg_replace(array('/(_)(.)/e'), array("strtoupper('\\2')"), $string);
+        return @preg_replace(array('/(_)(.)/e'), array("strtoupper('\\2')"), $string);
     }
 
     static function underscore($string) {
