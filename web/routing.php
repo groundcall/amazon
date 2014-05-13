f530c7bf3f21fb0070bf66fcc8c1dba18f229f83
Alexandru Keszeg <akeszeg@pitechnologies.ro>
2014-05-13 13:59:44 +0300

    fix routing with php dev server

--- web/routing.php
+++ web/routing.php
@@ -3,7 +3,7 @@
 if (file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
     return false; // serve the requested resource as-is.
 } else {
-    $_GET['url'] = @$_SERVER['REQUEST_URI'];
+    $_GET['url'] = @$_SERVER['PATH_INFO'];
 
     include_once 'index.php';
 }
